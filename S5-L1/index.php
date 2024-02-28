5c267f47f84c8aa9cc98641bc0b4643e872e46a4
rempairamore <rempairamore@gmail.com>
2024-02-28 14:07:18 +0000

    push iniziale

---
+++ S5-L1/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP S5-L1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Benvenuti</h1>
+    <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputnome" class="form-label">Nome</label>
+    <input type="text" class="form-control" id="inputnome">
+  </div>
+  <div class="col-md-6">
+    <label for="inputcognome" class="form-label">Cognome</label>
+    <input type="text" class="form-control" id="inputcognome">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+ 
+  
+  
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Invia dati al DB</button>
+  </div>
+</form>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
